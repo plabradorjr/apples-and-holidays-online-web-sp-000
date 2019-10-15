4f5752dff507b3d6ba8fdab4c7ea1243a64ae053
Pancho Labrador <plabradorjr@gmail.com>
2019-10-15 01:27:47 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -85,5 +85,8 @@ end
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
+  holiday_hash.each do |key1, val1|
+    binding.pry
 
+  end
 end
